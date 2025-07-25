64a2086adbd068e919d7337916759985d12f7228
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2025-07-25 10:45:07 +0300

    login

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,34 @@
+import 'package:cal/core/routing/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../Feature/Homepage/MyHomePage.dart';
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    //this arguments to be passed in any screen like this ( arguments as ClassName )
+    final arguments = settings.arguments;
+
+    switch (settings.name) {
+      // case Routes.onBoardingScreen:
+      //   return MaterialPageRoute(
+      //     builder: (_) => const OnboardingScreen(),
+      //   );
+      // case Routes.loginScreen:
+      //   return MaterialPageRoute(
+      //     builder: (_) => const LoginScreen(),
+      //   );
+
+      case Routes.homeScreen:
+        return MaterialPageRoute(builder: (_) => const MyHomePage());
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
